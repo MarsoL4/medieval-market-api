980b5aa94fb9eb0d05efd8ce76778bf988775bb5
MarsoL4 <enzogmarsola@gmail.com>
2025-04-24 11:01:45 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/com/fiap/medieval_market_api/MedievalMarketApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medieval_market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalMarketApiApplication.class, args);
+	}
+
+}
